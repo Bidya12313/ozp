546e081792cda3fffddad764e46ae54b780d6fc9
Bidya12313 <bogdan.matsenko@gmail.com>
2024-11-18 15:04:25 +0200

    created population script

--- database/population_db.py
+++ database/population_db.py
@@ -1,12 +1,23 @@
 from .engine import Base, db_engine
 from .system_models import Taxes
 from .budget_models import DailyBudget, GeneralBudget
-from .models import Users
+from .models import Users, Payers, Categories, Banks
+
+
+tables = tables=[
+        Users.__table__,
+        Payers.__table__,
+        Categories.__table__,
+        Banks.__table__,
+        Taxes.__table__,
+        DailyBudget.__table__,
+        GeneralBudget.__table__,
+        ]
 
 
 def create_table():
-    Base.metadata.drop_all(db_engine, tables=[Taxes.__table__])
-    Base.metadata.create_all(db_engine, tables=[Taxes.__table__])
+    Base.metadata.drop_all(db_engine, tables=tables)
+    Base.metadata.create_all(db_engine, tables=tables)
 
 
 if __name__ == '__main__':
